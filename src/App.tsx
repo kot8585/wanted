8ff86c16abc5b432fe4c79e755a759dd8fa5f8ca
HyoJeong Moon <57999419+kot8585@users.noreply.github.com>
2024-02-29 16:25:10 +0900

    디자인 수정

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import TodoList from "./components/List/TodoList";
 export default function App() {
   return (
     <>
-      <header className="text-3xl text-center font-Rosarivo">TO-DO LIST</header>
+      <header className="text-3xl text-center font-Rosarivo mb-5">
+        TO-DO LIST
+      </header>
       <AddTodo />
       <TodoList />
     </>
